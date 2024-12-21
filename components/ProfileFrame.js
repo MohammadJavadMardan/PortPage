45bd215d6f6007f24aef87c6db60c2aca32ec2c1
MohamadJavad Mardan <122433510+MohammadJavadMardan@users.noreply.github.com>
2024-12-21 12:28:17 +0330

    Update ProfileFrame.js

--- components/ProfileFrame.js
+++ components/ProfileFrame.js
@@ -4,7 +4,6 @@ function ProfileFrame() {
             <div data-name="profile-frame" className="profile-frame floating w-64 h-64 rounded-xl overflow-hidden mx-auto mt-20 relative">
                 <div className="absolute inset-0 bg-gradient-to-br from-purple-500/20 to-blue-500/20"></div>
                 <div className="w-full h-full flex items-center justify-center text-white">
-                    Upload your photo here
                 </div>
             </div>
         );
